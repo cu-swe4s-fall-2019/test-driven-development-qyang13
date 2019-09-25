638a0fac211f3da38757b3481f0885a56c9fd927
Qing Yang <qyang.kevin@gmail.com>
2019-09-25 15:51:53 -0600

    Added funtion GET_DATA, along with unit tests

--- get_data.py
+++ get_data.py
@@ -1,3 +1,32 @@
+import sys
 
 def read_stdin_col(col_num):
-    return None
+    '''
+    Takes column number and return an array of numbers from the
+    specific column of the specific input file. The file name is
+    specified through stdin
+    ---
+    Input: col_num
+    A 0-indexed column numbers
+    ---
+    Output: nums
+    A numerical array containing numbers from that spefied column
+    '''
+    try:
+        f = open(sys.stdin)
+    except  FileNotFoundError:
+        raise FileNotFoundError("The input file does not exist")
+        return None
+
+    nums = []
+
+    for l in f:
+        file_split = l.rstrip().split("\t")
+        try:
+            nums.append(int(file_split[col_num]))
+        except IndexError:
+            f.close()
+            raise IndexError("Index out of bound")
+
+    f.close()
+    return nums
